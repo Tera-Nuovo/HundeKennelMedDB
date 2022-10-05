0c15a4ddf3a50a0c48b0cd49ef93c7ab8bf9ce93
K.Ushiyama <K.Ushiyama@DESKTOP-8CLBLU0>
2022-10-05 19:07:06 +0900

    Add project files.

---
+++ HundeKennelMedDB1.0/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;  
+using System.Data.SqlClient;
+
+namespace HundeKennelMedDB1._0
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            DBAccess db = new DBAccess();
+
+            //insert dog into database
+            //specify id
+            SqlCommand inserCommand = new SqlCommand("INSERT INTO Dogs (id,Name) VALUES (@id,@Name)");
+            inserCommand.Parameters.AddWithValue("@id", 1);
+            inserCommand.Parameters.AddWithValue("@Name", "Bent");
+
+            db.executeQuery(inserCommand);
+            testlabel.Content = "dog inserted";
+        }
+    }
+}
