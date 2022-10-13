a52d5082277a077f6f475bd53b9eebc2ec1a9ce6
Christian Bech Fruchard <cbfr38694@edu.ucl.dk>
2022-10-13 11:52:48 +0200

    AddDog Update

--- HundeKennelMedDB1.0/MainWindow.xaml.cs
+++ HundeKennelMedDB1.0/MainWindow.xaml.cs
@@ -22,26 +22,42 @@ namespace HundeKennelMedDB1._0
     /// </summary>
     public partial class MainWindow : Window
     {
+        DBManager dBManager = new DBManager();
         public MainWindow()
         {
-
+            
             InitializeComponent();
+            
 
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
-            DBAccess db = new DBAccess();
+            
+            
+
+            // tilføj hund til database
+            dBManager.AddDog("10", "Bango", "", "1", "2", "M", "2", "130", "5", "5");
 
             //insert dog into database
             //specify id
-            SqlCommand inserCommand = new SqlCommand("INSERT INTO Dogs (id,Name) VALUES (@id,@Name)");
-            inserCommand.Parameters.AddWithValue("@id", 2);
-            inserCommand.Parameters.AddWithValue("@Name", "Ole");
+            /*SqlCommand inserCommand = new SqlCommand("INSERT INTO Dogs (id,Name,stambognr) VALUES (@id,@Name,@)");
+            inserCommand.Parameters.AddWithValue("@id", 3);
+            inserCommand.Parameters.AddWithValue("@Name", "Jane");
+            inserCommand.Parameters.AddWithValue("@stambognr", );
+            */
 
-            db.executeQuery(inserCommand);
-            testlabel.Content = "dog inserted";
+
+            
+            
+        }
+
+        private void DeleteDogs_Click(object sender, RoutedEventArgs e)
+        {
+            DBManager dBManager = new DBManager();
+
+            dBManager DeleteDogs() 
         }
     }
 }
